d231a4bb37fc4d8caa21ab0ed721b6d623de07f8
AcrobatAHA <ahanik94@gmail.com>
2017-12-19 14:37:50 +0600

    Add files via upload

---
+++ Find remainder without using modulas operator in python.py
@@ -0,0 +1,9 @@
+Num1 = int(input("Enter your dividend number :"))
+Num2 = int(input("Enter your divider number :"))
+
+Temp = Num1//Num2
+
+remainder_Num = Num1 - (Num2*Temp)
+
+print("The reminder of %d and %d is = %d"%(Num1,Num2,remainder_Num))
+
